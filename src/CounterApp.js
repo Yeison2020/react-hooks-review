b9b3d56e2ce6575ad5eb32f343f3634f6a8226f0
Yeison <enmanuelquiterios@gmail.com>
2021-10-16 14:41:57 -0400

    Completeded Module Unit testing

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
+// Note I needed to add this default value to make my test pass Learn why ?
 
-function CounterApp({ value }) {
+function CounterApp({ value = 10 }) {
   const [counter, setCounter] = useState(value);
 
   // Handle Functions
-  const handleAdd = (e) => {
+  const handleAdd = () => {
     // When you dont no know the name of the counter
     // No need to return;
     setCounter((c) => c + 1);
@@ -24,7 +25,6 @@ function CounterApp({ value }) {
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
-
       <button onClick={handleAdd}>+1</button>
       <button onClick={handleReset}>Reset</button>
       <button onClick={handleReduce}>-1</button>
--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -6,15 +6,45 @@ import "@testing-library/jest-dom";
 //----------Counter App testing
 
 describe("Pruebas en Counter Component", () => {
-  //first test
+  let wrapper = shallow(<CounterApp />);
+  beforeEach(() => {
+    wrapper = shallow(<CounterApp />);
+  });
+
+  //first test-----------------------------
+
   test("It should show counter componet matching with the Snapshot", () => {
-    const wrapper = shallow(<CounterApp />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  //-------------------------------------
 
   test("Tiene que mostrar el value default of 10", () => {
     const wrapper = shallow(<CounterApp value={10} />);
     const counterText = wrapper.find("h2").text().trim();
     expect(counterText).toBe("10");
+    //Here is why I got the Error The text returned string
+  });
+
+  test("Should increment the counter", () => {
+    wrapper.find("button").at(0).simulate("click");
+    const textCounter = wrapper.find("h2").text().trim();
+    expect(textCounter).toBe("11");
+  });
+
+  test("This should decrement the counter by 1", () => {
+    wrapper.find("button").at(2).simulate("click");
+    const counterBtn2 = wrapper.find("h2").text().trim();
+    expect(counterBtn2).toBe("9");
+  });
+
+  test("This button should Reset the value of the counter", () => {
+    wrapper.find("button").at(0).simulate("click");
+    const textCounter = wrapper.find("h2").text().trim();
+    console.log(textCounter);
+    //-------------------
+    wrapper.find("button").at(1).simulate("click");
+    const btnReset = wrapper.find("h2").text().trim();
+    expect(btnReset).toBe("0");
   });
 });
