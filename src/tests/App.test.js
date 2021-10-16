f390462178461b20a55bb92e3995758eace9d9dd
Yeison <enmanuelquiterios@gmail.com>
2021-10-16 12:53:48 -0400

    Added Enzyme Unit Testing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { getByText, render } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "../App";
 
 describe("Pruebas App", () => {
   test("Mostrar el mensaje Hola soy Yeison", () => {
     const salud = "Hola soy Yeison";
 
-    const { getByAltText } = render(<App salud={salud} />);
+    const { getByText } = render(<App salud={salud} />);
 
     expect(getByText(salud)).toBeInTheDocument();
   });
